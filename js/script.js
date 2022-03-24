89c942a6c0bb2c272608658a4ec772c053af2195
patrick <patrickbrasilr@gmail.com>
2022-03-24 17:17:20 -0300

    pequenos ajustes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 import initDigitacao from "./module/efeito-digitacao.js";
 import initMenuFixo from "./module/menu-fixo.js";
 import initMenuMobile from "./module/menu-mobile.js";
-import outsiteClick from "./module/outsideClick.js";
+
 
 initDigitacao();
 initMenuFixo();
 initMenuMobile();
-outsiteClick();
+
