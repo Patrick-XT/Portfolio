c11a038623ef4e388223d6d27fe34d373e8ac11a
patrick <patrickbrasilr@gmail.com>
2022-03-30 17:01:21 -0300

    slide

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
+import SlideNav from './module/slide-projetos.js';
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addArrow('.prev', '.next');
+slide.addControl('.custom-controls');
+
+
+// 
+// 
+// 
+// 
+// 
 import initDigitacao from "./module/efeito-digitacao.js";
 import initMenuFixo from "./module/menu-fixo.js";
 import initMenuMobile from "./module/menu-mobile.js";
-import Slide from "./module/slide-projetos.js"
-
-const slide = new Slide(".slide", ".slide-wrapper")
-
-slide.init()
-console.log(slide)
-
-
 
 initDigitacao();
 initMenuFixo();
 initMenuMobile();
-
