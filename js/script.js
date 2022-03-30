1731c619b73a6d4c43f64b12209ea5d1ebd45d3b
patrick <patrickbrasilr@gmail.com>
2022-03-30 14:39:03 -0300

    slide

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 import initDigitacao from "./module/efeito-digitacao.js";
 import initMenuFixo from "./module/menu-fixo.js";
 import initMenuMobile from "./module/menu-mobile.js";
+import Slide from "./module/slide-projetos.js"
+
+const slide = new Slide(".slide", ".slide-wrapper")
+
+slide.init()
+console.log(slide)
+
 
 
 initDigitacao();
