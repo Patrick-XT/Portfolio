cd4f5a962bbf72100fa87c413da7ee741fcd29d1
patrick <patrickbrasilr@gmail.com>
2022-04-29 15:00:26 -0300

    ajuste menu-ativo

--- js/module/menu-ativo.js
+++ js/module/menu-ativo.js
@@ -5,14 +5,14 @@ function menuAtivoScroll() {
   sectionsInView.forEach((section) => {
     const alturaSection = section.clientHeight;
     const alturaTop = section.getBoundingClientRect().top;
-    const menuAlt = document.querySelector(".navbar-default").clientHeight
+    const menuAlt = document.querySelector(".navbar-default").clientHeight;
     const href = section.getAttribute("id");
     const itemMenu = document.querySelector('a[href="#' + href + '"]');
     
 
     window.addEventListener("scroll", () => {
       const scrollTop = window.scrollY;
-      if (alturaTop - menuAlt < scrollTop && alturaTop + alturaSection - menuAlt > scrollTop) {
+      if ((alturaTop * 0.9)  - menuAlt < scrollTop && alturaTop + (alturaSection * 0.4) - menuAlt > scrollTop) {
         itemMenu.classList.add("active");
       } else{
         itemMenu.classList.remove("active");
